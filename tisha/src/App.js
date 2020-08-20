fe5f9463201910df346f31c7c643af0c3da2100a
lathisha <lathisha.n@gmail.com>
2020-08-20 15:14:37 -0700

    routing + pgs +comp folder created

--- tisha/src/App.js
+++ tisha/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Navbar } from 'react-bootstrap';
 import './App.css';
 
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 class App extends React.Component {
-  return (
-   
-  );
+  render() {
+    return (
+      <Router>
+        <Container>
+
+          <Navbar />
+
+          <Route exact path="/" component={About} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+
+          <Footer/>
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ tisha/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Navbar} from 'react-bootstrap';
+
+function navigationBar() {
+    return (
+        <Navbar expand="lg">
+            <Navbar.Brand>Tisha</Navbar.Brand>
+        </Navbar>
+    )
+};
+
+export default navigationBar;
