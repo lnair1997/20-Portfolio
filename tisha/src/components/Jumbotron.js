edd32acebcf51a0fb948e7fba0cea67c639eded8
lathisha <lathisha.n@gmail.com>
2020-08-31 22:01:46 -0700

    color scheme try out

--- tisha/src/components/Jumbotron.js
+++ tisha/src/components/Jumbotron.js
@@ -3,18 +3,24 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+const textColor = {
+    color: "#1B998B",
+    margin: "100px",
+}
 
 function JumbotronPic() {
     return (
-        <Jumbotron fluid={true} className="img">
-            <Row className="justify-content-center">
-                <Col className="text-center" md={9}>
-                    <h1 className="display-4 font-weight-bolder">Full Stack Developer</h1>
-                    <br></br>
-                    <h2 className="font-weight-light">HTML/CSS | Bootstrap | JavaScript | NodeJS | Express | MySQL | MongoDB | React |</h2>
-                </Col>
-            </Row>
-        </Jumbotron>
+        <div>
+            <Jumbotron fluid={true} className="img">
+                    <Row className="justify-content-center py-5" style={textColor}>
+                        <Col className="text-center" md={9}>
+                            <h1 className="display-4 font-weight-bolder">Full Stack Developer</h1>
+                            <br></br>
+                            <h2 className="font-weight-light text-light">HTML/CSS | Bootstrap | JavaScript | NodeJS | Express | MySQL | MongoDB | React |</h2>
+                        </Col>
+                    </Row>
+            </Jumbotron>
+        </div>
     )
 };
 
--- tisha/src/components/Navbar.js
+++ tisha/src/components/Navbar.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
+
 function NavigationBar() {
     return (
         <Navbar className="bg-transparent fixed-top scrolling-navbar">
             <Container className="container-md">
-                <Navbar.Brand className="text-light" href="/">TISHA</Navbar.Brand>
+                <Navbar.Brand className="" href="/"><strong>TISHA</strong></Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="navbar-toggle" />
                 <Navbar.Collapse id="navbar-toggle">
                     <Nav className="ml-auto">
-                        <Link className="nav-link text-light" to="/about">About</Link>
-                        <Link className="nav-link text-light" to="/projects">Projects</Link>
-                        <Link className="nav-link text-light" to="/contact">Contact</Link>
+                        <Link className="nav-link" to="/about"><strong>About</strong></Link>
+                        <Link className="nav-link" to="/projects"><strong>Projects</strong></Link>
+                        <Link className="nav-link" to="/contact"><strong>Contact</strong></Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
