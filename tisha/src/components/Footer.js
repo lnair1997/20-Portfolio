3d1ae1c20600773c33adb541e96faa5ad3beb804
lathisha <lathisha.n@gmail.com>
2020-09-28 17:15:04 -0700

    added my resume

--- tisha/src/components/Footer.js
+++ tisha/src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import resumePDF from "../lathisha-novoresume.pdf";
 
 function Footer() {
     return (
-        <footer class="page-footer">
-            <h3 class="footer-copyright text-center py-3">
-                <a href={'https://github.com/lnair1997'} target="_blank" rel="noopener noreferrer" class="footer-link"><FaGithub /></a>
-                <a href={'https://www.linkedin.com/in/lathishanair/'} target="_blank" rel="noopener noreferrer" class="footer-link"><FaLinkedin /></a>
-            </h3>
-            <FaLinkedin />
-            
+        <footer className="page-footer">
+            <h4 className="footer-copyright text-center py-3">
+                <a href={'https://github.com/lnair1997'} target="_blank" rel="noopener noreferrer" className="footer-link"><FaGithub /></a>
+                <a href={'https://www.linkedin.com/in/lathishanair/'} target="_blank" rel="noopener noreferrer" className="footer-link"><FaLinkedin /></a>
+                <a href={resumePDF} download={true} className="footer-link"><button className="btn btn-outline-secondary footer-link">RESUME</button></a>
+            </h4>
         </footer>
     );
 }
